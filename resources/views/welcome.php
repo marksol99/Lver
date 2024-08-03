4c0da8525a15adbcce90a764bf0aa178204b1255
Markus <markus_solheim@hotmail.com>
2024-08-03 18:34:27 +0200

    frontend 1 - forslag til hvordan det kan se ut på hovedsiden

---
+++ resources/views/welcome.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Marken</title>
+
+        <!-- Fonts -->
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+   </head>
+   <body>
+        <header>
+            <h1>Vølkommen</h1>
+        </header>
+
+        </form>
+
+   </body>
+</html>
